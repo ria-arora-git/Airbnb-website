28eb15b2c0ecd8481ae1c786f6d62018a75cbfac
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-03-03 07:30:30 +0530

    horizontal border line

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -5,7 +5,7 @@ const Search = () => {
      <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
         <div className="flex felx-row items-center justify-between"> 
             <div className="text-sm font-semibold px-6"> Anywhere</div>
-            <div className="hidden sm:block text-sm font-semibold px-6 border-x-{1px flex-1 text-center">Any Week</div>
+            <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">Any Week</div>
         </div>
      </div>   
      );
