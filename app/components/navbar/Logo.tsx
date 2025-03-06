7a7878576d0b96121de5ca81d1711fb8ab6884bf
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-03-06 21:40:22 +0530

    added search icon from react library

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/navigation';
 const Logo = () => {
     const router = useRouter();
     return(
-            <Image alt='Logo' className='hidden md:block cursor-pointer' height='100' width='120' src='/images/airbnb_logo.png'/>
+        <Image alt='Logo' className='hidden md:block cursor-pointer' height='100' width='100' src='/images/airbnb_logo.png'/>
     )
 }
 export default Logo;
