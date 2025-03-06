f60e77016a047f60aa4c774355d490de82e4ca83
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-03-07 04:32:57 +0530

    profile photo

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
     return ( 
@@ -11,6 +12,7 @@ const Navbar = () => {
                     <div className="flex felx-row items-center justify-between gap-3 md:gap-0">
                         <Logo />
                         <Search />
+                        <UserMenu /> 
                     </div>
                 </Container>
             </div>
